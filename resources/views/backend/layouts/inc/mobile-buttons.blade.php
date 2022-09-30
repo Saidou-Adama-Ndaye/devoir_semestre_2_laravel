c352a4dcf7b6d99aedad2fe86d523e379c0a1afa
Sajib <samiul.islam.unisel@gmail.com>
2022-10-01 01:57:12 +0600

    Backend template setup

---
+++ resources/views/backend/layouts/inc/mobile-buttons.blade.php
@@ -0,0 +1,9 @@
+<!-- Mobile Buttons Start -->
+<div class="mobile-buttons-container">
+    <!-- Menu Button Start -->
+    <a href="#" id="mobileMenuButton" class="menu-button">
+        <i data-cs-icon="menu"></i>
+    </a>
+    <!-- Menu Button End -->
+</div>
+<!-- Mobile Buttons End -->
