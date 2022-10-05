c2d5cf5eb01bbb7f1bc5c30e6172d0327e5f6834
Sajib <samiul.islam.unisel@gmail.com>
2022-10-06 04:31:39 +0600

    Product Model, Migration, factory and seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CategorySeeder::class,
             TestimonialSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
